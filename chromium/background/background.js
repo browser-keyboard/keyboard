d1201789ad4270f70690055046220030d0deee36
Bastian Neumann <neumann.bastian@gmail.com>
2023-12-24 16:07:18 +0100

    initial setup tries to load languages that are available on the system and create settings structure in local storage

--- chromium/background/background.js
+++ chromium/background/background.js
@@ -1,57 +1,5 @@
 chrome.browserAction.setBadgeBackgroundColor({color: [62, 62, 62, 255]});
 
-chrome.storage.local.get(["userOptions", "isActive", "languageList", "kStatus" ], function(data){
-  var isActive = data.isActive;
-  if(isActive === undefined){
-    chrome.storage.local.set({'isActive': true});
-  }
-
-  var userOptions = data.userOptions;
-  if(userOptions === undefined){
-    userOptions = {};
-    userOptions.show = 'always';
-    userOptions.capture = true;
-    userOptions.langToSave = true;
-    userOptions.size = "2x";
-    userOptions.color = "white";
-    chrome.storage.local.set({'userOptions': userOptions});
-  }
-
-  languageList = data.languageList;
-  if((languageList === undefined) || (!languageList[0])){
-    languageList = [ENGISHLAYOUT];
-    chrome.storage.local.set({'languageList': languageList});
-  }
-
-  var kStatus = data.kStatus;
-  if(kStatus === undefined){
-    var kStatus = {
-      shift:{
-				physical: false,
-				active: false
-			},
-      caps: {
-			active: false
-			},
-      addit: {
-				physical: false,
-				active: false
-      },
-      additLong: {
-				active: false
-      },
-      language: {
-				value: 0
-      }
-    };
-  }
-  kStatus.language.count = languageList.length;
-  if(!userOptions.langToSave)
-		kStatus.language.value = 0;
-  chrome.storage.local.set({'kStatus': kStatus});
-  f_updateBadgeList();
-});
-
 chrome.tabs.onActivated.addListener(function(info){
   f_sendKStatusOnActivate(info.tabId);
 });
@@ -81,13 +29,69 @@ chrome.runtime.onMessage.addListener(function(data, sender){
   };
 });
 
-function openOptionsOnFirstInstalling() {
-    if (!localStorage.getItem('was_installed')){
-      chrome.tabs.create({ "url": "chrome-extension://" + chrome.runtime.id + "/options/index.html"});
-      f_active(true);
-    }
+async function defaultOptionsOnFirstInstalling() {
+  if (!localStorage.getItem('was_installed')){
+    languageList = [];
+    
+    var localLanguageList = await new Promise(function(resolve, reject) {
+      chrome.i18n.getAcceptLanguages(function (localLanguageList) {
+        resolve(localLanguageList);
+      });
+    });
+    
+    const lang = await Promise.all(localLanguageList.map((localLanguage) => 
+      fetch('chrome-extension://' + chrome.runtime.id + '/content/languages/' + localLanguage + '.json')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (language) {
+        languageList.push(language);
+      })
+      .catch(function (error) {
+      })
+    ));
+
+
+    alert(languageList);
+    chrome.storage.local.set({'languageList': languageList});
+    
+    chrome.storage.local.set({'isActive': true});
+    
+    userOptions = {};
+    userOptions.show = 'on-active';
+    userOptions.capture = true;
+    userOptions.langToSave = true;
+    userOptions.size = "2x";
+    userOptions.color = "white";
+    chrome.storage.local.set({'userOptions': userOptions});
+    
+    var kStatus = {
+      shift:{
+        physical: false,
+        active: false
+      },
+      caps: {
+        active: false
+      },
+      addit: {
+        physical: false,
+        active: false
+      },
+      additLong: {
+        active: false
+      },
+      language: {
+        value: 0
+      }
+    };
+    kStatus.language.count = languageList.length;
+    chrome.storage.local.set({'kStatus': kStatus});
+    f_updateBadgeList();
+    f_active(true);
+    localStorage.setItem('was_installed', true);
+  }
 }
-setTimeout(openOptionsOnFirstInstalling, 500);
+defaultOptionsOnFirstInstalling();
 
 chrome.commands.onCommand.addListener(function(command) {
 	if(command == "toggle-feature-activision"){
